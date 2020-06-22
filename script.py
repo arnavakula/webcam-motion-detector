334590d316b39d81e5ab1dc1d6facec2b36af219
arnavakula <akula.arnav@gmail.com>
2020-06-22 14:58:09 -0700

    Implemented motion time

--- script.py
+++ script.py
@@ -1,12 +1,16 @@
 import cv2, time
+from datetime import datetime
 
 video = cv2.VideoCapture(0)
 first_frame = None
+status_list = [None, None]
+change_list = []
 video.read()
 time.sleep(2) #so the camera can properly register first_frame
 
 while True:
     check, frame = video.read()
+    status = 0
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0) #easier to interpret
 
@@ -24,9 +28,18 @@ while True:
     for contour in cnts:
         if cv2.contourArea(contour) < 1000:
             continue
-
+        
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+
+    status_list.append(status)
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        change_list.append(datetime.now)
+    elif status_list[-1] == 0 and status_list[-2] == 1:
+        change_list.append(datetime.now())
+
 
     # cv2.imshow('first', first_frame)
     cv2.imshow('Gray frame', gray_frame)
@@ -37,7 +50,11 @@ while True:
     key = cv2.waitKey(1)
 
     if key == ord('q'):
+        if status == 1: 
+            change_list.append(datetime.now())
         break
+
+print(change_list)
 
 video.release()
 cv2.destroyAllWindows()
