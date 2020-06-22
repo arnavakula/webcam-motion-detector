3fb2d029d1470a2d46f45c6b6ed4cbaa4b6b7c95
arnavakula <akula.arnav@gmail.com>
2020-06-22 10:47:18 -0700

    Implemented delta frame

--- script.py
+++ script.py
@@ -1,16 +1,24 @@
-import cv2
+import cv2, time
 
 video = cv2.VideoCapture(0)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+first_frame = None
 
+time.sleep(5)
 while True:
     check, frame = video.read()
-    faces = face_cascade.detectMultiScale(frame, scaleFactor = 1.05, minNeighbors = 5)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0) #easier to interprey
 
-    for x, y, w, h in faces:
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    if first_frame is None:
+        first_frame = gray_frame
+        continue
 
-    cv2.imshow('Motion Detector', frame)
+    df = cv2.absdiff(first_frame, gray_frame)
+
+    # cv2.imshow('first', first_frame)
+
+    cv2.imshow('Gray frame', gray_frame)
+    cv2.imshow('Delta frame', df)
 
     key = cv2.waitKey(1)
 
