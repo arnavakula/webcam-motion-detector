4cea6de30e4da0bbacc6e508e987fa89428379a3
arnavakula <akula.arnav@gmail.com>
2020-06-22 10:17:33 -0700

    Detect moving faces

--- script.py
+++ script.py
@@ -1,9 +1,14 @@
 import cv2
 
 video = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while True:
     check, frame = video.read()
+    faces = face_cascade.detectMultiScale(frame, scaleFactor = 1.05, minNeighbors = 5)
+
+    for x, y, w, h in faces:
+        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
     cv2.imshow('Motion Detector', frame)
 
